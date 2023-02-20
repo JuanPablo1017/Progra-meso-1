4210c64cecafa216be7830d38d140cd141b4339b
JuanPablo1017 <125237752+JuanPablo1017@users.noreply.github.com>
2023-02-20 16:27:39 -0600

    Add files via upload

---
+++ programa que solicite el numero de cursos.cpp
@@ -0,0 +1,32 @@
+// programa que solicite el numero de cursos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, total, primero;
+    cout << "Programa para saber que semestre lleva" << endl;
+    cout << "ingrese con numeros, cuantos curso lleva" << endl;
+    cin >> n1;
+    if (n1<=5)
+    {
+        cout << "Primer semestre" << endl;
+
+    }
+    else
+    {
+        cout << "No se sabe el semestre" << endl;
+    }
+    
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
